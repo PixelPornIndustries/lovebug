9154a3630d0a63f34e1007af98e3253b6388d2d4
PixelPornIndustries <129787095+PixelPornIndustries@users.noreply.github.com>
2023-04-04 14:11:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -150,7 +150,7 @@ loadModelFile = (fileName, path) => {
           clearcoatRoughness: 0.39,
           depthTest: true,
           depthWrite: true,
-	  blending: THREE.MultiplyBlending
+	  blending: THREE.AdditiveBlending
         });
       }
     });
